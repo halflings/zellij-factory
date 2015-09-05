9b9e786a20883e1fc9944c91bf6cb0b57b8cdbee
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-09-05 19:29:15 +0200

    Fixed some errors in the geometry, changed how lines are generated

--- geometry.py
+++ geometry.py
@@ -3,7 +3,7 @@ from math import cos, sin, pi, radians
 import numpy as np
 
 class ZellijSector(object):
-    def __init__(self, angle, r=500, w=400, incr=20):
+    def __init__(self, angle, r, incr):
         angle = radians(angle)
 
         self.angle = angle
@@ -12,15 +12,15 @@ class ZellijSector(object):
         self.normal = np.array([cos(angle + pi/2), sin(angle + pi/2)])
 
         self.lines = []
-        for x in range(-w, +w, incr):
-            p = np.array([x, 0])
+        for off in range(-r, r + 1, incr):
+            p = off * self.normal
             a, b = p - r * self.unit, p + r * self.unit
             self.lines.append((a, b))
 
 
 class Zellij(object):
-    def __init__(self, r=500, w=400, incr=20):
-        self.sectors = [ZellijSector(angle) for angle in range(0, 136, int(135/3))]
+    def __init__(self, r=200, incr=20):
+        self.sectors = [ZellijSector(angle, r, incr) for angle in range(0, 136, int(135/3))]
 
 if __name__ == '__main__':
     z = Zellij(angle=45)
