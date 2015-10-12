9d160954f1bed9c4878d478e475ed062a157dba9
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-10-12 15:40:00 +0200

    Fixed most intersection bugs (still a couple intersections missing)

--- geometry.py
+++ geometry.py
@@ -2,6 +2,26 @@ from math import cos, sin, pi, radians
 
 import numpy as np
 
+def perpendicular_segment(a) :
+    b = np.empty_like(a)
+    b[0] = -a[1]
+    b[1] = a[0]
+    return b
+
+def segment_intersection(a1, a2, b1, b2):
+    da = a2 - a1
+    db = b2 - b1
+    dp = a1 - b1
+    dap = perpendicular_segment(da)
+    denom = np.dot(dap, db)
+    if denom == 0:
+        return None
+    num = np.dot(dap, dp)
+    off_fact = (num / denom.astype(float))
+    if off_fact < 0 or off_fact > 1.0:
+        return None
+    return  b1 + off_fact * db
+
 class ZellijSector(object):
     def __init__(self, angle, r, incr):
         angle = radians(angle)
@@ -22,6 +42,7 @@ class Zellij(object):
     def __init__(self, r=90, incr=4):
         self.sectors = [ZellijSector(angle, r, incr) for angle in range(0, 136, int(135/3))]
         self.intersections = []
+
         for sector in self.sectors:
             for o_sector in self.sectors:
                 if sector == o_sector:
@@ -29,14 +50,15 @@ class Zellij(object):
                 collinearity = np.product([sector.unit, o_sector.unit])
                 if collinearity == 0:
                     continue
-                for line in sector.lines:
-                    for o_line in o_sector.lines:
-                        delta = np.product([o_line[0] - line[0], o_sector.unit]) / collinearity
-                        if delta < 0 or delta > 2 * sector.r:
-                            continue
-                        intersection = line[0] + delta * sector.unit
-                        #print(intersection)
-                        self.intersections.append(intersection)
+                self.intersect(sector, o_sector)
+
+    def intersect(self, sector, o_sector):
+        for line in sector.lines:
+            for o_line in o_sector.lines:
+                intersection = segment_intersection(line[0], line[1], o_line[0], o_line[1])
+                if intersection is None:
+                    continue
+                self.intersections.append(intersection)
 
 if __name__ == '__main__':
     z = Zellij(angle=45)
